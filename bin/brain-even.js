638303b1d5e4c761ca78860dd6d73137705c98f9
FreshinS <freshinsboost@gmail.com>
2023-11-30 11:28:06 +0300

    lint fixes

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,35 +1,34 @@
 #!/usr/bin/env node
 
-
-//Импорты
+// Импорты
 import readlineSync from 'readline-sync';
 import { welcome } from '../src/cli.js';
 
-//Настройка игры
-const n = 3; //Кол-во правильных ответов подряд
-const randMax = 100; //Максимальное случайное число
+// Настройка игры
+const a = 3; // Кол-во правильных ответов подряд
+const randMax = 100; // Максимальное случайное число
 
-const brain_even = (n, name) => {
-    let correct_count = 0;
-    let number = 0;
-    let answer = '';
-    let correct_answer;
-    while (correct_count < n) {
-        number = Math.round(Math.random() * randMax + 1); 
-        correct_answer = number % 2 === 0 ? 'yes' : 'no';
-        console.log(`Question: ${number}`);
-        answer = readlineSync.question('Your answer: ');
-        if (answer === correct_answer) {
-            correct_count += 1;
-            console.log('Correct!');
-        } else {
-            correct_count = 0;
-            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correct_answer}'.`);
-        }
+const brainEven = (n, name) => {
+  let correctCount = 0;
+  let number = 0;
+  let answer = '';
+  let correctAnswer;
+  while (correctCount < n) {
+    number = Math.round(Math.random() * randMax + 1);
+    correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+    console.log(`Question: ${number}`);
+    answer = readlineSync.question('Your answer: ');
+    if (answer === correctAnswer) {
+      correctCount += 1;
+      console.log('Correct!');
+    } else {
+      correctCount = 0;
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
     }
-    console.log(`Congratulations, ${name}!`);
-}
+  }
+  console.log(`Congratulations, ${name}!`);
+};
 
 const name = welcome();
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-brain_even(n, name);
+brainEven(a, name);
