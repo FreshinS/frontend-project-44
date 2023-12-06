ddfda5dc3e0ddc452d4590ba47a46b741e11d414
FreshinS <freshinsboost@gmail.com>
2023-12-06 11:29:00 +0300

    lint fixes

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,12 +2,25 @@
 
 // Импорты
 import readlineSync from 'readline-sync';
-import { welcome } from '../src/cli.js';
-import { getRandomInt } from '../src/cli.js';
+import { welcome, getRandomInt } from '../src/cli.js';
 
 // Настройка игры
 const a = 3; // Кол-во правильных ответов подряд
 const randMax = 100; // Максимальное случайное число
+
+const brainEven = (name) => {
+  const number = getRandomInt(randMax);
+  const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+  console.log(`Question: ${number}`);
+  const answer = readlineSync.question('Your answer: ');
+  if (answer === correctAnswer) {
+    console.log('Correct!');
+    return 1;
+  }
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+  console.log(`Let's try again, ${name}!`);
+  return 0;
+};
 
 const brainEvenStart = (n, name) => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
@@ -17,20 +30,7 @@ const brainEvenStart = (n, name) => {
     else return 0;
   }
   console.log(`Congratulations, ${name}!`);
-};
-
-const brainEven = (name) => {
-    const number = getRandomInt(randMax);
-    const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
-    console.log(`Question: ${number}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-      return 1;
-    }
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-    console.log(`Let's try again, ${name}!`);
-    return 0;
+  return 0;
 };
 
 const name = welcome();
