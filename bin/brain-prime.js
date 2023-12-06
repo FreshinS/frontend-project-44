e88b6149a9da24c253b0c3b7e6e84f67dc745bd8
FreshinS <freshinsboost@gmail.com>
2023-12-06 14:17:13 +0300

    lint fixes

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -9,30 +9,30 @@ const a = 3; // Кол-во правильных ответов подряд
 const randMax = 100;
 
 const checkPrime = (n) => {
-    let isPrime = true;
-    let i = 2;
-    while (i <= Math.sqrt(n) && isPrime === true) {
-        if (n % i === 0) {
-            isPrime = false;
-        }
-        i += 1;
+  let isPrime = true;
+  let i = 2;
+  while (i <= Math.sqrt(n) && isPrime === true) {
+    if (n % i === 0) {
+      isPrime = false;
     }
-    return isPrime ? 'yes' : 'no';
-}
+    i += 1;
+  }
+  return isPrime ? 'yes' : 'no';
+};
 
 const brainPrime = (name) => {
-    const number = getRandomInt(0, randMax);
-    const correctAnswer = checkPrime(number);
-    console.log(`Question: ${number}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer === correctAnswer) {
-        console.log('Correct!');
-        return 1;
-    }
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-    console.log(`Let's try again, ${name}!`);
-    return 0;
-}
+  const number = getRandomInt(0, randMax);
+  const correctAnswer = checkPrime(number);
+  console.log(`Question: ${number}`);
+  const answer = readlineSync.question('Your answer: ');
+  if (answer === correctAnswer) {
+    console.log('Correct!');
+    return 1;
+  }
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+  console.log(`Let's try again, ${name}!`);
+  return 0;
+};
 
 const gameName = brainPrime;
 
